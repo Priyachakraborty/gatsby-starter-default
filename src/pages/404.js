4fbcd597d30ee64b7840db82572fb94915351fff
Priya Chakraborty <89004448+Priyachakraborty@users.noreply.github.com>
2022-01-02 21:30:41 +0530

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -5,8 +5,8 @@ import Seo from "../components/seo"
 
 const NotFoundPage = () => (
   <Layout>
-    <Seo title="404: Not found" />
-    <h1>404: Not Found</h1>
+    <Seo title="404: Not found.Please try again later" />
+    <h1>404: Not Found.Please try again later</h1>
     <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
   </Layout>
 )
